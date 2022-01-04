018b6433f837f8bfe6647c81f9cd666b572f6e17
Adityanarayanojha <90720550+Adityanarayanojha@users.noreply.github.com>
2022-01-04 13:43:49 +0530

    Create most_frequent.py

---
+++ most_frequent.py
@@ -0,0 +1,11 @@
+import operator
+s = "Mississippi"
+def most_frequent_letter(string):
+  d = dict()
+  for letter in s:
+     if letter not in d:
+        d[letter] = s.count(letter)
+# Sort dictionary by values
+  ordered_answer = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
+  print (ordered_answer)
+print(most_frequent_letter(s))
